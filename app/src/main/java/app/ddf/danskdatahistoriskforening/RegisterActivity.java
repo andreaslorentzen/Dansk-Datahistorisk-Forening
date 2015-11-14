a1092714a32b062d2ce30886602e3dccba875969
Andreas Lorentzen <a@saloweb.dk>
2015-11-14 12:57:57 +0100

    register toolbar

--- app/src/main/java/app/ddf/danskdatahistoriskforening/RegisterActivity.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/RegisterActivity.java
@@ -2,12 +2,48 @@ package app.ddf.danskdatahistoriskforening;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Toolbar registerToolbar;
+    private EditText itemTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        registerToolbar.setTitle("Registrer genstand");
+        registerToolbar.setNavigationIcon(R.drawable.ic_close);
+        setSupportActionBar(registerToolbar);
+
+        itemTitle = (EditText) findViewById(R.id.itemTitle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_register_done:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
