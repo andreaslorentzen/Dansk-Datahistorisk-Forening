a451d7e05684c59f69bf4da027af2363784cbb67
Andreas Lorentzen <a@saloweb.dk>
2015-11-14 11:07:39 +0100

    Main toolbar

--- app/src/main/java/app/ddf/danskdatahistoriskforening/MainActivity.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/MainActivity.java
@@ -3,6 +3,7 @@ package app.ddf.danskdatahistoriskforening;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
 
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
