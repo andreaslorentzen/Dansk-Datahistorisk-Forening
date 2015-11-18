4179e59febca348591226030689111259eeb04cb
TotalOfferen <jonas@groggy.dk>
2015-11-18 09:52:16 +0100

    camera

--- app/src/main/java/app/ddf/danskdatahistoriskforening/RegisterItemFragment.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/RegisterItemFragment.java
@@ -1,16 +1,31 @@
 package app.ddf.danskdatahistoriskforening;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class RegisterItemFragment extends Fragment {
+import java.io.File;
+
+public class RegisterItemFragment extends Fragment implements View.OnClickListener{
+    static private final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    ImageButton cameraButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register_item, container, false);
+        View layout = inflater.inflate(R.layout.fragment_register_item, container, false);
+
+        cameraButton = (ImageButton) layout.findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(this);
+
+        return layout;
     }
 
     public void getTitle() {
@@ -21,4 +36,26 @@ public class RegisterItemFragment extends Fragment {
 
     public void getRecoording() {
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == cameraButton){
+            //http://developer.android.com/guide/topics/media/camera.html#intents
+            Uri fileUri;
+
+            fileUri = LocalMediaStorage.getOutputMediaFileUri(LocalMediaStorage.MEDIA_TYPE_IMAGE);
+
+            if(fileUri == null) {
+                Toast.makeText(getActivity(), "Could not access external storage", Toast.LENGTH_LONG);
+                return;
+            }
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
 }
