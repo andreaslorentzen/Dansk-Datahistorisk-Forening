a58ef7a58f4b3246b6b2b659a9ddef48fa3ac9c2
Andreas Lorentzen <a@saloweb.dk>
2015-11-22 13:43:13 +0100

    Add button

--- app/src/main/java/app/ddf/danskdatahistoriskforening/MainActivity.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/MainActivity.java
@@ -1,21 +1,33 @@
 package app.ddf.danskdatahistoriskforening;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton addActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
 
-        Intent i = new Intent(this, RegisterActivity.class);
-        startActivity(i);
+        addActivityButton = (FloatingActionButton) findViewById(R.id.fab);
+        addActivityButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == addActivityButton){
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        }
     }
 }
