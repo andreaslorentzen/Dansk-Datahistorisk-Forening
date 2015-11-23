3e0f11dd0f80b194f5b788dda0d76d16ad7f95df
Mathias Sondrup <matsondrup@gmail.com>
2015-11-23 10:59:26 +0100

    Added list of items to main acivity.

--- app/src/main/java/app/ddf/danskdatahistoriskforening/MainActivity.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/MainActivity.java
@@ -1,15 +1,33 @@
 package app.ddf.danskdatahistoriskforening;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     FloatingActionButton addActivityButton;
+    ListView itemList;
+    JSONArray items;
+    List<String> itemTitles;
+    private static final String URL = "http://78.46.187.172:4019/items";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
+
+        itemList = (ListView) findViewById(R.id.itemList);
+        itemTitles = new ArrayList<String>();
+
+        new AsyncTask() {
+            protected Object doInBackground(Object... arg0) {
+                BufferedReader br;
+                try {
+                    br = new BufferedReader(new InputStreamReader(new URL(URL).openStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String linje = br.readLine();
+                    while (linje != null) {
+                        sb.append(linje + "\n");
+                        linje = br.readLine();
+                    }
+                    return sb.toString();
+                } catch (IOException e){
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Object resultat) {
+                if (resultat != null){
+                    String data = (String) resultat;
+                    System.out.println("data = " + data);
+
+                    try {
+                        items = new JSONArray(data);
+
+                        for (int n = 0; n<items.length(); n++) {
+                            JSONObject item = items.getJSONObject(n);
+                            itemTitles.add(item.optString("itemheadline", "(ukendt)"));
+
+                            System.out.println("ihead = " + item.optString("itemheadline"));
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    updateItemList();
+                }
+            }
+        }.execute();
 
         addActivityButton = (FloatingActionButton) findViewById(R.id.fab);
         addActivityButton.setOnClickListener(this);
@@ -29,5 +93,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i = new Intent(this, RegisterActivity.class);
             startActivity(i);
         }
+    }
+
+    public void updateItemList(){
+        itemList.setAdapter(new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, itemTitles));
     }
 }
