9c237343362a03f69c864e61afb82ef9d68cc349
Mathias Sondrup <matsondrup@gmail.com>
2015-12-30 16:29:04 +0100

    #2 coppied all functionality from the old dao

--- app/src/main/java/app/ddf/danskdatahistoriskforening/IDAO.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/IDAO.java
@@ -4,4 +4,7 @@ import android.content.Context;
 
 public interface IDAO {
     int saveItemToDB(Context context, Item item);
+    String getOverviewFromBackend();
+    String getDetailsFromBackEnd(String detailsURI);
+
 }
--- app/src/main/java/app/ddf/danskdatahistoriskforening/tempDAO.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/tempDAO.java
@@ -1,7 +1,128 @@
 package app.ddf.danskdatahistoriskforening;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by mathias on 30/12/15.
  */
-public class tempDAO {
+public class tempDAO implements IDAO {
+    private static final String API = "http://msondrup.dk/api/v1";
+    private static final String userIDString = "?userID=56837dedd2d76438906140";
+
+    @Override
+    public int saveItemToDB(Context context, Item item) {
+        return 0;
+    }
+
+    @Override
+    public String getOverviewFromBackend() {
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new InputStreamReader(new URL(API + "/items" + userIDString).openStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String getDetailsFromBackEnd(String detailsURI) {
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new InputStreamReader(new URL(API + detailsURI + userIDString).openStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private class HttpPostItem extends AsyncTask<Item, Void, String> {
+        @Override
+        protected String doInBackground(Item... params) {
+            Log.d("fejl", "start post");
+            Item item = params[0];
+            Log.d("fejl", item.getItemHeadline());
+            String requestUrl = API + "/items?itemheadline=" + item.getItemHeadline() + "&itemdescription=" +  item.getItemHeadline();
+            //TODO build entire request from variables with stringbuilder
+
+            InputStream is = null;
+            String response = "";
+
+            try {
+                URL url = new URL(requestUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+
+                // Starts the query
+                conn.connect();
+                int responseCode = conn.getResponseCode();
+                Log.d("DDF", "The response is: " + responseCode + conn.getResponseMessage());
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                response = readIt(is, 2000);
+            }
+            catch (Exception e){
+                //TODO do something useful maybe
+                response = e.getMessage();
+                Log.d("DDF", response);
+            }
+            finally {
+                if(is != null){
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response){
+            System.out.println(response);
+
+            //TODO handle failure
+
+            //TODO extract itemid for updates
+
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
 }
