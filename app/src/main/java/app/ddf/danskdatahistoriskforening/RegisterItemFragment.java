668a5138b6b7c65fb690fb670880cf056e6b21df
TotalOfferen <jonas@groggy.dk>
2016-01-05 16:14:26 +0100

    #1 HorizontalScrollView med flere billeder. Thumbnails for at omgå OutOfMemoryException

--- app/src/main/java/app/ddf/danskdatahistoriskforening/RegisterItemFragment.java
+++ app/src/main/java/app/ddf/danskdatahistoriskforening/RegisterItemFragment.java
@@ -2,6 +2,8 @@ package app.ddf.danskdatahistoriskforening;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,19 +14,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class RegisterItemFragment extends Fragment implements View.OnClickListener{
 
     ImageButton cameraButton;
     ImageButton micButton;
-    ImageView imageView;
+    //ImageView imageView;
     RadioGaga rg;
     EditText itemTitle;
+    LinearLayout imageContatiner;
+    ArrayList<Uri> imageUris;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,9 +40,14 @@ public class RegisterItemFragment extends Fragment implements View.OnClickListen
         cameraButton.setOnClickListener(this);
         micButton =  (ImageButton) layout.findViewById(R.id.micButton);
         micButton.setOnClickListener(this);
-        imageView = (ImageView) layout.findViewById(R.id.imageView);
+        //imageView = (ImageView) layout.findViewById(R.id.imageView);
         itemTitle = (EditText) layout.findViewById(R.id.itemTitle);
         rg = new RadioGaga();
+
+        //((HorizontalScrollView) layout.findViewById(R.id.horizontalScrollView)).setFillViewport(true);
+        imageContatiner = (LinearLayout) layout.findViewById(R.id.imageContainer);
+        imageUris = new ArrayList<>();
+
         return layout;
     }
 
@@ -46,17 +58,21 @@ public class RegisterItemFragment extends Fragment implements View.OnClickListen
         rg.requestStop();
     }
 
-
-    Uri fileUri;
-
     @Override
     public void onClick(View v) {
         if(v == cameraButton){
             //http://developer.android.com/guide/topics/media/camera.html#intents
-            fileUri = LocalMediaStorage.getOutputMediaFileUri(LocalMediaStorage.MEDIA_TYPE_IMAGE);
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-            getActivity().startActivityForResult(intent, RegisterActivity.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            Uri fileUri = LocalMediaStorage.getOutputMediaFileUri(LocalMediaStorage.MEDIA_TYPE_IMAGE);
+            if(fileUri != null) {
+                imageUris.add(fileUri);
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                getActivity().startActivityForResult(intent, RegisterActivity.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+            else{
+                Toast.makeText(getActivity(), "Der opstod en fejl ved oprettelse af billedet, sørg for at SD kortet er tilgængeligt og prøv igen.", Toast.LENGTH_LONG).show();
+            }
         }
         if(v == micButton){
             rg.execute();
@@ -68,8 +84,19 @@ public class RegisterItemFragment extends Fragment implements View.OnClickListen
         if (requestCode == RegisterActivity.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 // Image captured and saved to fileUri specified in the Intent
-                imageView.setImageURI(fileUri);
-             //   Toast.makeText(getActivity(),  Toast.LENGTH_LONG).show();
+
+                ImageView image = new ImageView(getActivity());
+                LinearLayout.LayoutParams sizeParameters = new LinearLayout.LayoutParams(150, 250);
+                image.setLayoutParams(sizeParameters);
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap thumbnail = BitmapFactory.decodeFile(imageUris.get(imageUris.size() - 1).getPath(), options);
+                image.setImageBitmap(thumbnail);
+
+                //image.setImageURI(imageUris.get(imageUris.size()-1));
+                imageContatiner.addView(image);
+
             } else if (resultCode == Activity.RESULT_CANCELED) {
                 // User cancelled the image capture
                 Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
